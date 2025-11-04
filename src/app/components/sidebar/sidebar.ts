feb1b890093f6ae143424874c3e023437d587a8a
Miguel Hernandez <jesmiguelhdzg@gmail.com>
2025-11-04 09:55:54 -0600

    sidebar con flowbite

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,5 +1,6 @@
-import { Component, signal, ChangeDetectionStrategy, ElementRef, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, afterNextRender} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,42 +8,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {
-    '(document:click)': 'onDocumentClick($event)'
-  }
 })
 export class Sidebar {
-  private readonly elementRef = inject(ElementRef);
-  
-  protected readonly isSidebarOpen = signal(false);
-  protected readonly isDropdownUserMenuOpen = signal(false);
 
-  protected toggleSidebar(): void {
-    this.isSidebarOpen.update(open => !open);
-  }
-
-  protected closeSidebar(): void {
-    this.isSidebarOpen.set(false);
-  }
-
-  protected toggleDropdownUserMenu(): void {
-    this.isDropdownUserMenuOpen.update(open => !open);
-  }
-
-  protected onDocumentClick(event: MouseEvent): void {
-    const clickedElement = event.target as HTMLElement;
-    const dropdownButton = this.elementRef.nativeElement.querySelector('#user-menu-button');
-    const dropdownMenu = this.elementRef.nativeElement.querySelector('#user-dropdown');
-
-    // Si el click fue fuera del botón y del menú, cerrar el dropdown
-    if (
-      this.isDropdownUserMenuOpen() &&
-      dropdownButton &&
-      dropdownMenu &&
-      !dropdownButton.contains(clickedElement) &&
-      !dropdownMenu.contains(clickedElement)
-    ) {
-      this.isDropdownUserMenuOpen.set(false);
-    }
+  constructor() {
+    afterNextRender(() => initFlowbite());
   }
 }
