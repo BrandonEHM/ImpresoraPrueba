2da643dde12cebb3fcf3b91890e25d89a33b844c
BrandonEHM <brandonhermart@gmail.com>
2025-11-05 12:22:58 -0600

    Productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,9 @@
 import { Routes } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
 
-
-import { NuevoProducto } from './components/nuevo-producto/nuevo-producto';
-import { EditarProducto } from './components/editar-producto/editar-producto';
 import { Paginacion } from './components/paginacion/paginacion';
-import { InterfazEditarProductos } from './components/interfaz-editar-productos/interfaz-editar-productos';
-
 import { Productos } from './components/productos/productos';
 export const routes: Routes = [
-  /*{ path: '', redirectTo: '/interfaz-editar-productos', pathMatch: 'full' },
-  { path: 'interfaz-editar-productos', component: InterfazEditarProductos },
-  { path: 'nuevo-producto', component: NuevoProducto },
-  { path: 'editar-producto', component: EditarProducto },
-  { path: 'paginacion', component: Paginacion },*/
 
   {
     path: 'admin',
@@ -24,18 +14,6 @@ export const routes: Routes = [
         path: 'productos',
         component: Productos,
         title: 'Productos'
-      },
-      
-      {
-        path: 'nuevo-producto',
-        component: NuevoProducto,
-        title: 'NuevoProducto'
-      },
-
-      {
-        path: 'editar-producto',
-        component: EditarProducto,
-        title: 'NuevoProducto'
       },
 
       {
