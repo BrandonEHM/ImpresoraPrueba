934ce8a3e0808ab8e5bbf8c0bf548295f4622b37
Miguel Hernandez <jesmiguelhdzg@gmail.com>
2025-11-13 15:18:16 -0600

    agregación modo osucuro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
-import { Component, signal, afterNextRender } from '@angular/core';
+import { Component, signal, afterNextRender, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { ThemeService } from './services/theme.service';
+
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('izc-pos-frontend');
+  private readonly themeService = inject(ThemeService); // Inicializa el servicio
 
   constructor() {
     afterNextRender(() => {
