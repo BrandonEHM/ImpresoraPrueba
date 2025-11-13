951ad9446b08e62f0e7deedda693ebb05edd6af6
Guillermo <pinguino2223@gmail.com>
2025-11-13 15:54:11 -0600

    feature/agenda

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,11 @@
 import { Component, signal, afterNextRender, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
-<<<<<<< HEAD
-import { FullCalendarModule } from '@fullcalendar/angular';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FullCalendarModule],
-=======
 import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
->>>>>>> 8a9a00c297f8bc33ff893b29993d93258bc2fb7a
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
